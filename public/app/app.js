2df68eaa19a4c3d49efcda5c297162c5a5ff9072
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-06 22:50:25 -0300

    feat(utils): Limiting operations

--- public/app/app.js
+++ public/app/app.js
@@ -1,12 +1,19 @@
 import "./utils/array-helpers.js"; // instancia e carregamento do prototype em memória
 import { log } from "./utils/promise-helpers.js";
 import { notasService as service } from "./notas/service.js";
+import { takeUntil } from "./utils/operators.js";
 
 /**
- * Calculo
+ * Limitando a 3 vezes a possibilidade de clique no botão que fazem acesso a API
  */
-document.querySelector("#myButton").onclick = () =>
+const operation = takeUntil(3, () =>
   service
     .sumItems("2143")
-    .then(log)
-    .catch(log);
+    .then(console.log)
+    .catch(console.log)
+);
+
+/**
+ * Clique do botão que realiza o calculo
+ */
+document.querySelector("#myButton").onclick = operation;
--- public/app/utils/operators.js
+++ public/app/utils/operators.js
@@ -6,9 +6,13 @@
  * Uma função retornada tem a capacidade de lembrar do contexto no qual ela foi declarada, ou seja,
  * ela lembrará do contexto dos parâmetros passado para a função que a criou.
  *
- * SumItems recebe como parâmetro um código que devolve uma nova função que recebe como parâmetro as notas
  */
 
+/**
+ * SumItems recebe como parâmetro um código que devolve uma nova função que recebe como parâmetro as notas
+ *
+ * @param  {...any} fns N...Functions
+ */
 export const pipe = (...fns) => value =>
   fns.reduce((previousValue, fn) => fn(previousValue), value);
 
@@ -18,5 +22,19 @@ export const pipe = (...fns) => value =>
  * nova função utilize como contexto. Porém, como declaramos a função através de arrow function que não aceita a
  * modificação do seu contexto, simplesmente passamos null.
  * Mesmo que tivéssemos passado outro valor ele seria ignorado.
+ *
+ * @param {Function} fn Target function
+ * @param  {...any} params Parms
  */
 export const partialize = (fn, ...params) => fn.bind(null, ...params);
+
+/**
+ * Define o limite máximo de chamadas a api.
+ *
+ * A função retornada lembrará dos parâmetros recebidos pela função takeUntil que a retornou.
+ * Essa memória ocorre devido ao suporte de closure na linguagem JavaScript.
+ *
+ * @param {Number} times Quantidade de vezes
+ * @param {Function} fn Callback function
+ */
+export const takeUntil = (times, fn) => () => times-- > 0 && fn();
