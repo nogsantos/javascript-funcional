fd5d8dfe90ed05fe05cb4bfda06e1da2b6c84787
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-09 14:51:19 -0300

    refactor(app): Operations as pipe

--- public/app/app.js
+++ public/app/app.js
@@ -1,16 +1,23 @@
 import "./utils/array-helpers.js"; // instancia e carregamento do prototype em memória
 import { log } from "./utils/promise-helpers.js";
 import { notasService as service } from "./notas/service.js";
-import { takeUntil, debounceTime } from "./utils/operators.js";
+import {
+  takeUntil,
+  debounceTime,
+  partialize,
+  pipe
+} from "./utils/operators.js";
 
-const action = debounceTime(
-  500,
-  takeUntil(3, () =>
-    service
-      .sumItems("2143")
-      .then(console.log)
-      .catch(console.log)
-  )
+const operations = pipe(
+  partialize(takeUntil, 3),
+  partialize(debounceTime, 500)
+);
+
+const action = operations(() =>
+  service
+    .sumItems("2143")
+    .then(console.log)
+    .catch(console.log)
 );
 
 /**
