db497c27427ad944e9e4883965a06ec7986152bb
Fabricio Nogueira <nogsantos@gmail.com>
2019-03-15 12:00:10 -0300

    feat(promises): Delay function
    Make a delay in milliseconds betweens `then()` on promises

--- public/app/utils/promise-helpers.js
+++ public/app/utils/promise-helpers.js
@@ -30,3 +30,20 @@ export const timeoutPromise = (milliseconds, promise) => {
   );
   return Promise.race([timeout, promise]);
 };
+
+/**
+ * Permite realizar um atraso entre as chamadas encadeada à função `then()`
+ *
+ * A função delay recebe como parâmetro o tempo em milissegundos do delay e possui como retorno outra função.
+ * Esta nova função retornada, que lembrará do tempo a ser respeitado, recebe como parâmetro o resultando
+ * da chamada à then() anterior.
+ * Essa chamada pode ou não retornar um valor. Então, a função ao ser invocada retornará uma nova Promise
+ * que será resolvida através de uma chamada de setTimeout.
+ * Quando resolvida, passará o valor recebido da chamada then() anterior para sua próxima chamada encadeada.
+ *
+ * @param {Number} milliseconds
+ */
+export const delay = milliseconds => data =>
+  new Promise((resolve, reject) =>
+    setTimeout(() => resolve(data), milliseconds)
+  );
